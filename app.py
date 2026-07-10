50bad7d5e0ea84483292edcf3008ae9774bbad5a
Your Name <your@email.com>
2026-07-10 23:40:11 +0530

    Refactor search flow

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from dotenv import load_dotenv
 import requests
 import os
@@ -8,6 +8,7 @@ load_dotenv()
 app = Flask(__name__)
 
 api_key = os.getenv("OMDB_API_KEY")
+
 
 def format_votes(votes):
     votes = int(votes.replace(",", ""))
@@ -22,35 +23,53 @@ def format_votes(votes):
 
     return str(votes)
 
+
 @app.route("/", methods=["GET", "POST"])
 def home():
-
-    movie = None
 
     if request.method == "POST":
 
         movie_name = request.form.get("movie", "").strip()
 
-        url = f"http://www.omdbapi.com/?t={movie_name}&apikey={api_key}"
+        if movie_name:
+            return redirect(
+                url_for(
+                    "movie_page",
+                    movie_name=movie_name
+                )
+            )
 
-        try:
-            response = requests.get(url, timeout=5)
-            response.raise_for_status()
+    return render_template("index.html")
 
-            data = response.json()
 
-            if data["Response"] == "True":
-                data["formatted_votes"] = format_votes(data["imdbVotes"])
-                movie = data
+@app.route("/movie/<movie_name>")
+def movie_page(movie_name):
 
-        except requests.RequestException:
-            pass
+    movie = None
+
+    url = f"http://www.omdbapi.com/?t={movie_name}&apikey={api_key}"
+
+    try:
+
+        response = requests.get(url, timeout=5)
+        response.raise_for_status()
+
+        data = response.json()
+
+        if data["Response"] == "True":
+
+            data["formatted_votes"] = format_votes(data["imdbVotes"])
+
+            movie = data
+
+    except requests.RequestException:
+        pass
 
     return render_template(
-        "index.html",
+        "movie.html",
         movie=movie
     )
 
+
 if __name__ == "__main__":
-    app.run()
-    
+    app.run(debug=True)
